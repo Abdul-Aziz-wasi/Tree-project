2bda1014edb22ea25613917f9336025b16f4190a
Abdul Aziz <abdulaziz758015@gmail.com>
2025-05-24 23:59:04 +0600

    addea

--- src/components/Plant.jsx
+++ src/components/Plant.jsx
@@ -1,7 +1,33 @@
 import React from 'react';
+import Swal from 'sweetalert2';
 
 const Plant = ({tree}) => {
-    const {image,email,name,health}=tree
+    const {_id,image,email,name,health}=tree;
+
+    const handleDelete =(_id)=>{
+        console.log(_id)
+        Swal.fire({
+  title: "Are you sure?",
+  text: "You won't be able to revert this!",
+  icon: "warning",
+  showCancelButton: true,
+  confirmButtonColor: "#3085d6",
+  cancelButtonColor: "#d33",
+  confirmButtonText: "Yes, delete it!"
+}).then((result) => {
+  if (result.isConfirmed) {
+    fetch(`http://localhost:5173/myplants`,{
+        method:'Delete'
+    }).then(res=>res.json()).then(data=>console.log(data))
+    Swal.fire({
+      title: "Deleted!",
+      text: "Your file has been deleted.",
+      icon: "success"
+    });
+  }
+});
+
+    }
     return (
       <div className="card bg-base-100 w-96 shadow-sm">
   <figure>
@@ -16,8 +42,8 @@ const Plant = ({tree}) => {
     </h2>
     <p>{email}</p>
     <div className="card-actions justify-end">
-      <div className="badge badge-outline">Delet</div>
-      <div className="badge badge-outline">Edit</div>
+      <div onClick={()=>{handleDelete(_id)}} className="badge badge-outline">Delet</div>
+      <div  className="badge badge-outline">Edit</div>
     </div>
   </div>
 </div>
